f6a353f8609082d9c5e00317acef851ca7529fa8
Haroon Khan <mrharoonkhan11@gmail.com>
2025-07-20 21:50:32 +0300

    Secret hidden

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUTn0kBo1k4XvJ4UkNCQj2onw-LqHF9D0",
-  authDomain: "impostor-game-30bda.firebaseapp.com",
-  databaseURL: "https://impostor-game-30bda-default-rtdb.firebaseio.com",
-  projectId: "impostor-game-30bda",
-  storageBucket: "impostor-game-30bda.firebasestorage.app",
-  messagingSenderId: "348170722047",
-  appId: "1:348170722047:web:2c491dc8d1204128e10aca"
+  
 };
 
 firebase.initializeApp(firebaseConfig);
